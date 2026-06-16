bc6b01aeb86dca1bf21e4e93b2a2d6ba379499dc
urazco-maker <urazco@icloud.com>
2026-06-16 13:20:43 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,15 @@
-print("Doa Bot Başlatıldı")
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+TOKEN = 8890631109:AAE0qz3y40hoCG5k6XKNjbTt8-BFotNGfjc
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "🚀 Doa Nasdaq Bot Aktif!"
+    )
+
+app = Application.builder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()

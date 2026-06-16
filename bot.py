ab9bfb34aeb9f762e41c19c7c4d0ab0b29d2e335
urazco-maker <urazco@icloud.com>
2026-06-17 02:49:33 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,15 @@
-print("Doa Bot Çalışıyor")
+import os
+import requests
+
+TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
+CHAT_ID = "8662141509"
+
+def send_message(text):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    requests.post(url, data={
+        "chat_id": CHAT_ID,
+        "text": text
+    })
+
+send_message("🚀 Doa Bot aktif!")
+print("Gönderildi")
